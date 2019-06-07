e15e854c81dde91c85640feff9fb53ad92abe8cd
Abdul Sammad <abdulsammad200@gmail.com>
2019-06-07 12:00:38 +0500

    Inital Commit

---
+++ server/config/keys.js
@@ -0,0 +1,7 @@
+***REMOVED***
+***REMOVED*** process.env.DISCORD_BOT_TOKEN,
+  redisHost: process.env.REDIS_HOST,
+  redisPort: process.env.REDIS_PORT,
+***REMOVED*** process.env.MONGO_CONNECTION_STRING,
+  sessionSecret: process.env.SESSION_SECRET
+***REMOVED***
