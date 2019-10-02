03a5ac971ebc6000d3c5d692982ea4d87a0e2a10
Light Yagami <abdulsammad200@gmail.com>
2019-10-02 19:52:36 +0500

    Updated validation

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,6 @@
 ***REMOVED***
 ***REMOVED*** process.env.MONGO_CONNECTION_STRING,
 ***REMOVED*** process.env.DISCORD_BOT_TOKEN,
-  aldoviaAPIKey: process.env.ALDOVIA_API_KEY
+  aldoviaAPIKey: process.env.ALDOVIA_API_KEY,
+  botEnv: 'production',
 ***REMOVED***
