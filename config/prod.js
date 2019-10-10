e63b3b2334549f2930f82100dd202c4c4a441e33
Light Yagami <abdulsammad200@gmail.com>
2019-10-10 12:34:38 +0500

    Added music

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,7 @@
 ***REMOVED***
 ***REMOVED*** process.env.MONGO_CONNECTION_STRING,
 ***REMOVED*** process.env.DISCORD_BOT_TOKEN,
+  youtubeAPIKey: process.env.YOUTUBE_API_KEY,
   aldoviaAPIKey: process.env.ALDOVIA_API_KEY,
   botEnv: process.env.BOT_ENV,
 ***REMOVED***
