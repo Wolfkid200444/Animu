7d41f6eea0bb91e762039f9b05f8d1cf809feee3
Light Yagami <abdulsammad200@gmail.com>
2019-10-13 17:54:59 +0500

    Added user Command

---
+++ util/util.js
@@ -0,0 +1,10 @@
+***REMOVED***
+  trimArray: function(arr, maxLen = 10) {
+    if (arr.length > maxLen) {
+      const len = arr.length - maxLen;
+      arr = arr.slice(0, maxLen);
+      arr.push(`${len} more...`);
+    }
+    return arr;
+  },
+***REMOVED***
