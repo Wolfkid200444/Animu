827590d696b706617375a5ee1f0876b104b2c384
Light Yagami <abdulsammad200@gmail.com>
2019-12-10 19:53:30 +0500

    Added color Command

---
+++ commands/Image Manipulation/color.js
@@ -0,0 +1,34 @@
+const { Command } = require('klasa');
+const { createCanvas } = require('canvas');
+
+module.exports = class extends Command {
+  constructor(...args) {
+    super(...args, {
+      runIn: ['text', 'group'],
+      requiredPermissions: ['EMBED_LINKS'],
+      cooldown: 10,
+      description: 'Get image of a color',
+      usage: '<color:string>',
+    });
+  }
+
+  async run(msg, [color]) {
+    const canvas = createCanvas(250, 250);
+    const ctx = canvas.getContext('2d');
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.arc(
+      canvas.width / 2,
+      canvas.height / 2,
+      canvas.height / 2,
+      0,
+      Math.PI * 2
+    );
+    ctx.closePath();
+    ctx.clip();
+    ctx.fill(0, 0, 250, 250);
+    return msg.send({
+      files: [{ attachment: canvas.toBuffer(), name: 'color.png' }],
+    });
+  }
+***REMOVED***
