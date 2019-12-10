b5148d8ecc2df7fa5797e833f606496efdf76015
Light Yagami <abdulsammad200@gmail.com>
2019-12-10 20:12:23 +0500

    Added create-qr-code Command

---
+++ commands/Image Manipulation/create-qr-code.js
@@ -0,0 +1,24 @@
+const { Command } = require('klasa');
+
+module.exports = class extends Command {
+  constructor(...args) {
+    super(...args, {
+      runIn: ['text', 'group'],
+      requiredPermissions: ['EMBED_LINKS'],
+      cooldown: 10,
+      description: 'Convert text to QR Code',
+      usage: '<text:string>',
+    });
+  }
+
+  async run(msg, [text]) {
+    return msg.send({
+      files: [
+        {
+          attachment: `http://api.qrserver.com/v1/create-qr-code/?data=${text}`,
+          name: 'qr-code.png',
+        },
+      ],
+    });
+  }
+***REMOVED***
