5b664bb4c975e7a28ce1a84c7247f73709f807d5
Light Yagami <abdulsammad200@gmail.com>
2019-12-10 21:59:04 +0500

    Added iFunny Command

---
+++ commands/Image Manipulation/ifunny.js
@@ -0,0 +1,43 @@
+const { Command } = require('klasa');
+const { createCanvas, loadImage } = require('canvas');
+const path = require('path');
+
+module.exports = class extends Command {
+  constructor(...args) {
+    super(...args, {
+      runIn: ['text', 'group'],
+      requiredPermissions: ['EMBED_LINKS'],
+      cooldown: 10,
+      description: "Draw iFunny logo on a user's avatar",
+      usage: '<user:member>',
+    });
+  }
+
+  async run(msg, [user]) {
+    const image = user.user.displayAvatarURL({ format: 'png', size: 512 });
+    try {
+      const base = await loadImage(
+        path.join(__dirname, '..', '..', 'images', 'ifunny.png')
+      );
+      const data = await loadImage(image);
+      const canvas = createCanvas(data.width, data.height);
+      const ctx = canvas.getContext('2d');
+      ctx.drawImage(data, 0, 0);
+      ctx.fillStyle = '#181619';
+      ctx.fillRect(0, canvas.height - base.height, canvas.width, base.height);
+      ctx.drawImage(
+        base,
+        canvas.width - base.width,
+        canvas.height - base.height
+      );
+      const attachment = canvas.toBuffer();
+      if (Buffer.byteLength(attachment) > 8e6)
+        return msg.reply('Resulting image was above 8 MB.');
+      return msg.send({ files: [{ attachment, name: 'ifunny.png' }] });
+    } catch (err) {
+      return msg.reply(
+        `Oh no, an error occurred: \`${err.message}\`. Try again later!`
+      );
+    }
+  }
+***REMOVED***
