e511cdc1967631cc7ed77d0c45830cc62d5ca492
Light Yagami <abdulsammad200@gmail.com>
2019-12-10 22:09:43 +0500

    Added minecraft-skin Command

---
+++ commands/Image Manipulation/minecraft-skin.js
@@ -0,0 +1,33 @@
+const { Command } = require('klasa');
+const axios = require('axios');
+
+module.exports = class extends Command {
+  constructor(...args) {
+    super(...args, {
+      runIn: ['text', 'group'],
+      requiredPermissions: ['EMBED_LINKS'],
+      cooldown: 10,
+      description: "View a user's Minecraft Skin",
+      usage: '<user:string> <face|front|frontfull|head|bust|full|skin>',
+      usageDelim: ' ',
+    });
+  }
+
+  async run(msg, [user, type]) {
+    try {
+      const search = await axios.get(
+        `https://api.mojang.com/users/profiles/minecraft/${user}`
+      );
+      if (search.status === 204) return msg.send('Could not find any results.');
+      return msg.send({
+        files: [
+          `https://visage.surgeplay.com/${type}/512/${search.data.id}.png`,
+        ],
+      });
+    } catch (err) {
+      return msg.reply(
+        `Oh no, an error occurred: \`${err.message}\`. Try again later!`
+      );
+    }
+  }
+***REMOVED***
