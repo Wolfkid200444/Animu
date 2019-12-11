aea6b246c83f37d66fcf141dfa00c821de936125
Light Yagami <abdulsammad200@gmail.com>
2019-12-11 12:49:00 +0500

    Added rip Command

--- util/Canvas.js
+++ util/Canvas.js
@@ -1,4 +1,17 @@
 module.exports = class CanvasUtil {
+  static greyscale(ctx, x, y, width, height) {
+    const data = ctx.getImageData(x, y, width, height);
+    for (let i = 0; i < data.data.length; i += 4) {
+      const brightness =
+        0.34 * data.data[i] + 0.5 * data.data[i + 1] + 0.16 * data.data[i + 2];
+      data.data[i] = brightness;
+      data.data[i + 1] = brightness;
+      data.data[i + 2] = brightness;
+    }
+    ctx.putImageData(data, x, y);
+    return ctx;
+  }
+
   static shortenText(ctx, text, maxWidth) {
     let shorten = false;
     while (ctx.measureText(text).width > maxWidth) {
