0b4ea58cf7b58e9166bcbdb334046cf24a671004
Light Yagami <abdulsammad200@gmail.com>
2019-12-11 11:06:24 +0500

    Added bob-ross Command

---
+++ commands/Avatar Manipulation/bob-ross.js
@@ -0,0 +1,38 @@
+const { Command } = require('klasa');
+const { createCanvas, loadImage } = require('canvas');
+const path = require('path');
+
+module.exports = class extends Command {
+  constructor(...args) {
+    super(...args, {
+      runIn: ['text', 'group'],
+      requiredPermissions: ['EMBED_LINKS'],
+      cooldown: 10,
+      description: "Draw a user's avatar on Bobb Ross's Canvas",
+      usage: '<user:member>',
+    });
+  }
+
+  async run(msg, [user]) {
+    const image = user.user.displayAvatarURL({ format: 'png', size: 512 });
+    try {
+      const base = await loadImage(
+        path.join(__dirname, '..', '..', 'images', 'bob-ross.png')
+      );
+      const avatar = await loadImage(image);
+      const canvas = createCanvas(base.width, base.height);
+      const ctx = canvas.getContext('2d');
+      ctx.fillStyle = 'white';
+      ctx.fillRect(0, 0, base.width, base.height);
+      ctx.drawImage(avatar, 15, 20, 440, 440);
+      ctx.drawImage(base, 0, 0);
+      return msg.send({
+        files: [{ attachment: canvas.toBuffer(), name: 'bob-ross.png' }],
+      });
+    } catch (err) {
+      return msg.reply(
+        `Oh no, an error occurred: \`${err.message}\`. Try again later!`
+      );
+    }
+  }
+***REMOVED***
