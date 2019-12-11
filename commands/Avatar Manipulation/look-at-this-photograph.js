bd3da93f523e87b5184e531c91bc7ea99fe651ca
Light Yagami <abdulsammad200@gmail.com>
2019-12-11 12:10:21 +0500

    Added look-at-this-photgraph Command

---
+++ commands/Avatar Manipulation/look-at-this-photograph.js
@@ -0,0 +1,49 @@
+const { Command } = require('klasa');
+const { createCanvas, loadImage } = require('canvas');
+const path = require('path');
+
+module.exports = class extends Command {
+  constructor(...args) {
+    super(...args, {
+      runIn: ['text', 'group'],
+      requiredPermissions: ['EMBED_LINKS'],
+      cooldown: 10,
+      description: "Draws a user's avatar over Nickelback's photograph.",
+      usage: '<user:member>',
+    });
+  }
+
+  async run(msg, [user]) {
+    const image = user.user.displayAvatarURL({ format: 'png', size: 512 });
+    try {
+      const base = await loadImage(
+        path.join(
+          __dirname,
+          '..',
+          '..',
+          'images',
+          'look-at-this-photograph.png'
+        )
+      );
+      const avatar = await loadImage(image);
+      const canvas = createCanvas(base.width, base.height);
+      const ctx = canvas.getContext('2d');
+      ctx.drawImage(base, 0, 0);
+      ctx.rotate(-13.5 * (Math.PI / 180));
+      ctx.drawImage(avatar, 280, 218, 175, 125);
+      ctx.rotate(13.5 * (Math.PI / 180));
+      return msg.send({
+        files: [
+          {
+            attachment: canvas.toBuffer(),
+            name: 'look-at-this-photograph.png',
+          },
+        ],
+      });
+    } catch (err) {
+      return msg.reply(
+        `Oh no, an error occurred: \`${err.message}\`. Try again later!`
+      );
+    }
+  }
+***REMOVED***
