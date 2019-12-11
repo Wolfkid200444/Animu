175367a4f2df3a3f0abeaa4c16e977f2f1fff0ab
Light Yagami <abdulsammad200@gmail.com>
2019-12-11 11:01:45 +0500

    Added avatar-fusion Command

---
+++ commands/Avatar Manipulation/avatar-fusion.js
@@ -0,0 +1,39 @@
+const { Command } = require('klasa');
+const { createCanvas, loadImage } = require('canvas');
+
+module.exports = class extends Command {
+  constructor(...args) {
+    super(...args, {
+      runIn: ['text', 'group'],
+      requiredPermissions: ['EMBED_LINKS'],
+      cooldown: 10,
+      description: 'Overlay one avatar on top of another',
+      usage: '<overlay:user> <base:user>',
+      usageDelim: ' ',
+    });
+  }
+
+  async run(msg, [overlay, base]) {
+    const overlayAvatarURL = overlay.displayAvatarURL({
+      format: 'png',
+      size: 512,
+    });
+    const baseAvatarURL = base.displayAvatarURL({ format: 'png', size: 512 });
+    try {
+      const baseAvatar = await loadImage(baseAvatarURL);
+      const overlayAvatar = await loadImage(overlayAvatarURL);
+      const canvas = createCanvas(baseAvatar.width, baseAvatar.height);
+      const ctx = canvas.getContext('2d');
+      ctx.globalAlpha = 0.5;
+      ctx.drawImage(baseAvatar, 0, 0);
+      ctx.drawImage(overlayAvatar, 0, 0, baseAvatar.width, baseAvatar.height);
+      return msg.send({
+        files: [{ attachment: canvas.toBuffer(), name: 'avatar-fusion.png' }],
+      });
+    } catch (err) {
+      return msg.reply(
+        `Oh no, an error occurred: \`${err.message}\`. Try again later!`
+      );
+    }
+  }
+***REMOVED***
