c6fec50b1cad609ed6edc62da90fe051b687019f
Light Yagami <abdulsammad200@gmail.com>
2019-12-14 20:37:37 +0500

    Locked several commands behind lite & plus

--- inhibitors/iskeyvalid.js
+++ inhibitors/iskeyvalid.js
@@ -1,5 +1,6 @@
 const { Inhibitor } = require('klasa');
 const { model } = require('mongoose');
+const _ = require('lodash');
 const redis = require('redis');
 const bluebird = require('bluebird');
 
@@ -8,7 +9,55 @@ bluebird.promisifyAll(redis.RedisClient.prototype);
 const redisClient = redis.createClient();
 
 module.exports = class extends Inhibitor {
-  async run() {
+  async run(message, command) {
+    // Lite+ Categories
+    const liteCategories = [
+      'Avatar Manipulation',
+      'Image Manipulation',
+      'Music',
+      'Multi-Player Games',
+      'Search',
+    ];
+
+    // Plus+ Categories
+    const plusCategories = ['Verified'];
+
+    // Lite+ Commands
+    const liteCommands = ['read', 'watch'];
+
+    // Lite+ Commands
+    const plusCommands = ['pixiv'];
+
+    const guildTier = await redisClient.hgetAsync(
+      'guild_tiers',
+      message.guild.id
+    );
+
+    // Validation
+    if (
+      _.includes(command.fullCategory[0], liteCategories) &&
+      guildTier === 'free'
+    )
+      return 'This command is for Lite and Above tiers';
+
+    if (
+      _.includes(command.fullCategory[0], plusCategories) &&
+      _.includes(['free', 'lite'], guildTier)
+    )
+      return 'This command is for Plus and Above tiers';
+
+    if (
+      _.includes(command.name, liteCommands) &&
+      _.includes(['free'], guildTier)
+    )
+      return 'This command is for Lite and Above tiers';
+
+    if (
+      _.includes(command.name, plusCommands) &&
+      _.includes(['free', 'lite'], guildTier)
+    )
+      return 'This command is for Plus and Above tiers';
+
     return false;
   }
 
