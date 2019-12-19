f374ce9a2fa319c9dfdc656cd4e5acefaa2da5bc
Light Yagami <abdulsammad200@gmail.com>
2019-12-19 19:56:47 +0500

    Added logging to deposit

--- tasks/deposit.js
+++ tasks/deposit.js
@@ -13,15 +13,20 @@ module.exports = class extends Task {
     const bankaccounts = await BankAccount.find({}).exec();
 
     bankaccounts.forEach(async account => {
+      console.log(account);
       account.deposits.forEach(async deposit => {
         if (deposit.daysLeft === 1) {
           const inv = await Inventory.findOne({ memberID: account.memberID });
+          console.log(inv);
           await inv.addCoins(deposit.coins);
+                    console.log(inv);
+
           // eslint-disable-next-line require-atomic-updates
-          deposit.coins = 0;
+          deposit = undefined;
         }
         if (deposit.daysLeft > 0) deposit.daysLeft--;
       });
+      console.log(account);
       await account.save();
     });
   }
