46affd773a84c7c420bab2ff8e17058cc3b57ef3
Light Yagami <abdulsammad200@gmail.com>
2020-01-03 12:17:14 +0500

    Added expiry to logs

--- models/Log.js
+++ models/Log.js
@@ -8,6 +8,7 @@ const logSchema = new Schema({
     default: Date.now,
   },
   data: Schema.Types.Mixed,
+  expire_at: { type: Date, default: Date.now(), expires: 60 * 60 * 24 * 30 },
 });
 
 model('Log', logSchema);
