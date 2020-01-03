0b7e0333fc0b8bfd1ba8892d659524720bfe9fb5
Light Yagami <abdulsammad200@gmail.com>
2020-01-03 12:07:30 +0500

    Added D100 Command

---
+++ commands/Roleplay/d100.js
@@ -0,0 +1,26 @@
+const { Command } = require('klasa');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = class extends Command {
+  constructor(...args) {
+    super(...args, {
+      runIn: ['text', 'dm', 'group'],
+      requiredPermissions: ['EMBED_LINKS'],
+      bucker: 3,
+      cooldown: 10,
+      description: 'Roll D100 Dice',
+      extendedHelp: 'Roll a D100 dice',
+    });
+  }
+
+  async run(msg) {
+    return msg.sendEmbed(
+      new MessageEmbed()
+        .setTitle(`${msg.member.displayName} rolled D100`)
+        .setDescription(
+          `Rolling a D100... 🎲 **${Math.ceil(Math.random() * 100)}**`
+        )
+        .setColor('#2196f3')
+    );
+  }
+***REMOVED***
