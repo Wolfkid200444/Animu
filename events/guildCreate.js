95030b0caa20012fd1642aedd69b5423ab3deb43
Light Yagami <abdulsammad200@gmail.com>
2020-01-13 11:17:47 +0500

    Added guildCreate message

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -10,6 +10,26 @@ const redisClient = redis.createClient();
 
 module.exports = class extends Event {
   async run(guild) {
+    const firstChannel = guild.channels
+      .filter(
+        ch =>
+          ch.permissionsFor(guild.me).has('SEND_MESSAGES') && ch.type === 'text'
+      )
+      .sort((a, b) => a.rawPosition - b.rawPosition)
+      .first();
+
+    firstChannel.send(`
+Thanks for having me here! <:wave:605078389925085206>
+My name's Animu and my purpose is to make your server moar better <a:YAY_1:619057676336496661>
+> To get list of commands, use \`-help\` or visit this link: <https://aldovia.moe/animu-commands/>
+> To get help regarding a specific command, use \`-help command\`
+> To view your profile, use \`-p\`
+> To view your inventory, use \`-inv\`
+> Visit this link for some useful guides: <https://aldovia.moe/tag/animu/>
+> Download Animu Companion App from here: <https://github.com/LightYagami200/Animu-Companion/releases>\n
+Need help? Join the support server: discord.gg/JGsgBsN
+    `);
+
     const existingGuild = await Guild.findOne({ guildID: guild.id }).exec();
 
     if (existingGuild) return;
