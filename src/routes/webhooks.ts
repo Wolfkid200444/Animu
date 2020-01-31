ecfd3ceff9934be3248d95591729bdb32f7a38e6
Light Yagami <abdulsammad200@gmail.com>
2020-01-31 21:16:27 +0500

    Updated webhooks

--- src/routes/webhooks.ts
+++ src/routes/webhooks.ts
@@ -1,14 +1,14 @@
 import { Application } from 'express';
-import { model } from 'mongoose';
-import { IInventoryModel } from '../models/Inventory';
+// import { model } from 'mongoose';
+// import { IInventoryModel } from '../models/Inventory';
 import { KlasaClient } from 'klasa';
 
-//@ts-ignore
-const Inventory = <IInventoryModel>model('Inventory');
+// //@ts-ignore
+// const Inventory = <IInventoryModel>model('Inventory');
 
 module.exports = (app: Application, client: KlasaClient) => {
   app.post('/webhooks', (req, res) => {
-    console.log(req.body);
     res.json({ status: 'active' });
+    console.log(req.body);
   });
 };
