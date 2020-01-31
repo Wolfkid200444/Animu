096a66b93a14fdfb111f90d55eaff8ed780bf8a7
Light Yagami <abdulsammad200@gmail.com>
2020-01-31 15:57:36 +0500

    Added try catch to toxicity filters to remove log spam

--- src/monitors/toxicityFilters.ts
+++ src/monitors/toxicityFilters.ts
@@ -1,5 +1,5 @@
-import { MessageEmbed } from 'discord.js';
-import { Monitor, MonitorStore } from 'klasa';
+import { MessageEmbed, TextChannel } from 'discord.js';
+import { Monitor, MonitorStore, KlasaMessage } from 'klasa';
 import Perspective from 'perspective-api-client';
 import { model } from 'mongoose';
 import { perspectiveAPIKey } from '../config/keys';
@@ -19,7 +19,7 @@ module.exports = class extends Monitor {
     });
   }
 
-  async run(message) {
+  async run(message: KlasaMessage) {
     if (
       !_.includes(
         ['plus', 'pro'],
@@ -28,69 +28,79 @@ module.exports = class extends Monitor {
     )
       return;
 
+    if (message.content.trim.length < 1) return;
+
     // Filters
     const filters = [];
 
-    if (message.guild.settings.toxicityFilters.toxicity)
+    if (message.guild.settings.get('toxicityFilters.toxicity'))
       filters.push('TOXICITY');
-    if (message.guild.settings.toxicityFilters.severeToxicity)
+    if (message.guild.settings.get('toxicityFilters.severeToxicity'))
       filters.push('SEVERE_TOXICITY');
-    if (message.guild.settings.toxicityFilters.profanity)
+    if (message.guild.settings.get('toxicityFilters.profanity'))
       filters.push('PROFANITY');
-    if (message.guild.settings.toxicityFilters.identityAttack)
+    if (message.guild.settings.get('toxicityFilters.identityAttack'))
       filters.push('IDENTITY_ATTACK');
-    if (message.guild.settings.toxicityFilters.sexuallyExplicit)
+    if (message.guild.settings.get('toxicityFilters.sexuallyExplicit'))
       filters.push('SEXUALLY_EXPLICIT');
-    if (message.guild.settings.toxicityFilters.flirtation)
+    if (message.guild.settings.get('toxicityFilters.flirtation'))
       filters.push('FLIRTATION');
-    if (message.guild.settings.toxicityFilters.threat) filters.push('THREAT');
+    if (message.guild.settings.get('toxicityFilters.threat'))
+      filters.push('THREAT');
 
     if (filters.length === 0) return;
 
-    const res = await perspective.analyze(message.cleanContent, {
-      attributes: filters,
-    });
+    try {
+      const res = await perspective.analyze(message.cleanContent, {
+        attributes: filters,
+      });
 
-    const filtersFailed = [];
+      const filtersFailed = [];
 
-    filters.forEach(filter => {
-      if (res.attributeScores[filter].summaryScore.value >= 0.8) {
-        filtersFailed.push(filter);
-      }
-    });
+      filters.forEach(filter => {
+        if (res.attributeScores[filter].summaryScore.value >= 0.8) {
+          filtersFailed.push(filter);
+        }
+      });
 
-    if (filtersFailed.length === 0) return;
+      if (filtersFailed.length === 0) return;
 
-    await new Log({
-      guildID: message.guild.id,
-      event: 'toxicMessage',
-      data: {
-        authorID: message.author.id,
-        channelID: message.channel.id,
-        content: message.content,
-        createdAt: message.createdAt,
-        edits: message.edits,
-        id: message.id,
-        type: message.type,
-        filtersFailed: filtersFailed,
-      },
-    }).save();
+      await new Log({
+        guildID: message.guild.id,
+        event: 'toxicMessage',
+        data: {
+          authorID: message.author.id,
+          channelID: message.channel.id,
+          content: message.content,
+          createdAt: message.createdAt,
+          edits: message.edits,
+          id: message.id,
+          type: message.type,
+          filtersFailed: filtersFailed,
+        },
+      }).save();
 
-    if (message.guild.settings.logChannels.reports)
-      message.guild.channels
-        .get(message.guild.settings.logChannels.reports)
-        .send(
-          new MessageEmbed({
-            title: `Message by ${message.author.tag}`,
-            description: `The following message failed **${
-              filtersFailed.length
-            }** filters:\n\n\`\`\`${message.cleanContent}\`\`\`\n(Sent in ${
-              message.channel
-            })\n\nFilters Failed: *${filtersFailed.join(', ')}*`,
-            color: 0x2196f3,
-          })
+      if (message.guild.settings.get('logChannels.reports')) {
+        const logChannel = message.guild.channels.get(
+          message.guild.settings.get('logChannels.reports')
         );
 
-    if (message.guild.settings.deleteToxicMessages) await message.delete();
+        if (logChannel instanceof TextChannel)
+          logChannel.send(
+            new MessageEmbed({
+              title: `Message by ${message.author.tag}`,
+              description: `The following message failed **${
+                filtersFailed.length
+              }** filters:\n\n\`\`\`${message.cleanContent}\`\`\`\n(Sent in ${
+                message.channel
+              })\n\nFilters Failed: *${filtersFailed.join(', ')}*`,
+              color: 0x2196f3,
+            })
+          );
+      }
+
+      if (message.guild.settings.get('deleteToxicMessages'))
+        await message.delete();
+    } catch (e) {}
   }
 };
