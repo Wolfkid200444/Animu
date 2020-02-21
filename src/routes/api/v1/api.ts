8557371fa037ed49a2e81b4f46460d22b34d2bb4
Light Yagami <abdulsammad200@gmail.com>
2020-02-21 18:38:39 +0500

    Added staff route to API v1

--- src/routes/api/v1/api.ts
+++ src/routes/api/v1/api.ts
@@ -3,6 +3,7 @@ import express, { Application, Request, Response, NextFunction } from 'express';
 import { KlasaClient } from 'klasa';
 import redis from 'redis';
 import bluebird from 'bluebird';
+import moment from 'moment';
 
 // Init
 const api = express.Router();
@@ -15,7 +16,7 @@ const version = 1.0;
 module.exports = (app: Application, client: KlasaClient) => {
   // Middlewares
   api.use(
-    ['/guilds/*'],
+    ['/guilds*'],
     async (req: Request, res: Response, next: NextFunction) => {
       const token = req.headers.authorization;
 
@@ -37,7 +38,7 @@ module.exports = (app: Application, client: KlasaClient) => {
   );
 
   api.use(
-    ['/guilds/:id/*'],
+    ['/guilds/:id*'],
     async (req: Request, res: Response, next: NextFunction) => {
       // Fetching Guild
       const guild = client.guilds.get(req.params.id);
@@ -82,17 +83,77 @@ module.exports = (app: Application, client: KlasaClient) => {
     });
   });
 
+  // Provides Stats of a guild
+  // -> Active Now Count
+  // -> Growth Rate
+  // -> Join Rate
+  // ---------------
+  // Query Params:
+  // growthCycle - number of days to show growthRate for (int)
+  // joinRate - number of days to show joinRate for (int)
   api.get('/guilds/:id/stats', (req, res) => {
+    const growthRate = [];
+    const joinRate = [];
+
+    const growthCycle = parseInt(req.query.growthCycle) || 7;
+    const joinCycle = parseInt(req.query.joinCycle) || 7;
+
+    const activeNowCount = req.guild.members.filter(
+      m =>
+        m.presence.status === 'online' ||
+        m.presence.status === 'idle' ||
+        m.presence.status === 'dnd'
+    ).size;
+
+    for (let i = 0; i < growthCycle; i++) {
+      const date = moment().subtract(i, 'days');
+      growthRate.push(
+        req.guild.members.filter(m =>
+          moment(m.joinedAt).isSameOrBefore(date, 'day')
+        ).size
+      );
+    }
+
+    for (let i = 0; i < joinCycle; i++) {
+      const date = moment().subtract(i, 'days');
+      joinRate.push(
+        req.guild.members.filter(m => moment(m.joinedAt).isSame(date, 'day'))
+          .size
+      );
+    }
+
     return res.json({
       stats: {
-        active: 10,
+        activeNowCount,
+        growthRate,
+        joinRate,
       },
     });
   });
 
+  // Fetch IDs of Staff members (members with moderation-related permissions)
+  api.get('/guilds/:id/staff', (req, res) => {
+    const staff = req.guild.members
+      .filter(
+        m =>
+          m.hasPermission('ADMINISTRATOR') ||
+          m.hasPermission('MANAGE_GUILD') ||
+          m.hasPermission('BAN_MEMBERS') ||
+          m.hasPermission('KICK_MEMBERS')
+      )
+      .array()
+      .map(m => m.id);
+
+    return res.json({
+      staff,
+    });
+  });
+
   // ? Routes to Add:
-  // - GET /guids/:id/stats => Returns Growth stats, join rate, etc
-  // - GET /guilds/:id/staff => Returns users with moderation related perms
+  // - GET /guilds/:id/members => Return member ids of all the members in a guild
+  // - GET /guilds/:id/members/:id => Return info about a member in a guild
+  // - POST /guilds/:id/members/:id/kick => Kick the member
+  // - POST /guilds/:id/members/:id/ban => Ban the member
   // - GET /guilds/:id/notifications => Return notifications such as reports, Updates, etc
   // - GET /users/:id => Return info about the user (must be the logged in user)
   // - GET /users/:id/token => Returns token for the logged in user alongside QR Code
